a760a9165317ac417b887a6ecbb53beb5a542204
mobeets <mobeets@gmail.com>
2018-12-16 11:48:27 -0500

    more text, also presets

--- static/js/main.js
+++ static/js/main.js
@@ -2,7 +2,7 @@
 // var nNeurons = 3; // implicit assumed for now
 var stimuli = [];
 var nStimuli = 8;
-var nTimesteps = 15;
+var nTimesteps = 12;
 var lines = [];
 var graph_layout = [];
 var update = [];
@@ -17,12 +17,15 @@ var t0 = 2; // time of stimulus onset
 var tau = 2; // tau for stimulus response
 var ciRate = 0; // rate for CI
 var ciTau = 1; // tau for CI
+var stimPref1 = 0.0;
+var stimPref2 = 3.0;
+var stimPref3 = 6.0;
 
 function g(t,tau) { // evoked input gate
   // t is current time
   // tau is time constant of exponential
   // outputs value in [0,1]
-  var gate = 1 - Math.pow(Math.E, -t*tau);
+  var gate = 1 - Math.pow(Math.E, -t/tau);
   // constrain to be in [0,1]
   return Math.min(Math.max(gate, 0), 1);
 }
@@ -85,9 +88,14 @@ function gatherPsths(xs, ys, zs, ts) {
 }
 
 function make3Psths(stimuli, nTimesteps, returnLines) {
-  var sx = Math.PI/4;
-  var sy = 3*Math.PI/4;
-  var sz = Math.PI;
+  // preferred stimuli for each neuron
+  var sx = stimPref1*Math.PI/4;
+  var sy = stimPref2*Math.PI/4;
+  var sz = stimPref3*Math.PI/4;
+  $('#stimpref1').html(rad2deg(sx.toString()));
+  $('#stimpref2').html(rad2deg(sy.toString()));
+  $('#stimpref3').html(rad2deg(sz.toString()));
+
   var lines = [];
   var xss = [];
   var yss = [];
@@ -129,25 +137,16 @@ function make3Psths(stimuli, nTimesteps, returnLines) {
     tss.push(ts);
     lines.push(cdata);
   }
-  return {graph_lines: lines,
-    psth_lines: psth_obj.psth_lines,
-    psth_update: psth_obj.psth_update,
-    graph_update: {x: xss, y: yss, z: zss}};
-}
-
-function updateGraphs() {
-  var newdata = make3Psths(stimuli, nTimesteps);
-  Plotly.update('graph', newdata.graph_update);
-  Plotly.update('psths', newdata.psth_update);
-}
-
-function createGraphs() {
-  var curdata = make3Psths(stimuli, nTimesteps);
   graph_layout = {
+    title: 'Population activity',
     scene: {
       xaxis: {title: 'Neuron 1'},
       yaxis: {title: 'Neuron 2'},
-      zaxis: {title: 'Neuron 3'}
+      zaxis: {title: 'Neuron 3'},
+      camera: {
+          eye: { 
+                x: 2, y: 2, z: 0.1 }
+        }
     },
     height: 640,
   };
@@ -156,8 +155,24 @@ function createGraphs() {
     xaxis: {title: 'Time (t)'},
     yaxis: {title: 'Firing rate (r)'}
   };
-  Plotly.plot('graph', curdata.graph_lines, graph_layout);
-  Plotly.plot('psths', curdata.psth_lines, psth_layout);
+  return {graph_lines: lines,
+    psth_lines: psth_obj.psth_lines,
+    psth_update: psth_obj.psth_update,
+    graph_update: {x: xss, y: yss, z: zss},
+    graph_layout: graph_layout,
+    psth_layout: psth_layout};
+}
+
+function updateGraphs() {
+  var newdata = make3Psths(stimuli, nTimesteps);
+  Plotly.update('graph', newdata.graph_update, newdata.graph_layout);
+  Plotly.update('psths', newdata.psth_update, newdata.psth_layout);
+}
+
+function createGraphs() {
+  var curdata = make3Psths(stimuli, nTimesteps);
+  Plotly.plot('graph', curdata.graph_lines, curdata.graph_layout);
+  Plotly.plot('psths', curdata.psth_lines, curdata.psth_layout);
 }
 
 function makeStimuli(nStimuli) {
@@ -177,7 +192,7 @@ function changeB() {
   updateGraphs();
 }
 function changeTau() {
-  tau = parseFloat($("#slider-tau").val())/2.0;
+  tau = parseFloat($("#slider-tau").val())/3.0;
   updateGraphs();
 }
 function changeCiRate() {
@@ -185,15 +200,84 @@ function changeCiRate() {
   updateGraphs();
 }
 function changeCiTau() {
-  ciTau = parseFloat($("#slider-ci-tau").val())/2.0;
+  ciTau = parseFloat($("#slider-ci-tau").val())/3.0;
   updateGraphs();
 }
+function changeStimIndex() {
+  stimIndexForPsth = parseInt($("#slider-stim-index").val());
+  updateGraphs();
+}
+function changeStimPref1() {
+  stimPref1 = parseFloat($("#slider-thetapref-1").val());
+  updateGraphs();
+}
+function changeStimPref2() {
+  stimPref2 = parseFloat($("#slider-thetapref-2").val());
+  updateGraphs();
+}
+function changeStimPref3() {
+  stimPref3 = parseFloat($("#slider-thetapref-3").val());
+  updateGraphs();
+}
+function makeSnowflake() {
+  stimPref1 = 0;
+  stimPref2 = 3;
+  stimPref3 = 6;
+  ciRate = 0;
+  ciTau = 1.0;
+  a = 5;
+  b = 3;
+  tau = 2.0;
+  updateGraphs();
+}
+function makeGramophone() {
+  stimPref1 = 0;
+  stimPref2 = 3;
+  stimPref3 = 6;
+  ciRate = 4;
+  ciTau = 1.0;
+  a = 5;
+  b = 4;
+  tau = 3.0;
+  updateGraphs();
+}
+function makeHeadMassager() {
+  stimPref1 = 0;
+  stimPref2 = 3;
+  stimPref3 = 6;
+  ciRate = 9;
+  ciTau = 3.0;
+  a = 5;
+  b = 3;
+  tau = 1.0;
+  updateGraphs();
+}
+function makeSpider() {
+  stimPref1 = 0;
+  stimPref2 = 3;
+  stimPref3 = 6;
+  ciRate = 2;
+  ciTau = 3.0;
+  a = 5;
+  b = 3;
+  tau = 1.0;
+  updateGraphs();
+}
+
 function addHandlers() {
   $("#slider-a").click(changeA);
   $("#slider-b").click(changeB);
   $("#slider-tau").click(changeTau);
   $("#slider-ci").click(changeCiRate);
   $("#slider-ci-tau").click(changeCiTau);
+  $("#slider-stim-index").click(changeStimIndex);
+  $("#slider-thetapref-1").click(changeStimPref1);
+  $("#slider-thetapref-2").click(changeStimPref2);
+  $("#slider-thetapref-3").click(changeStimPref3);
+  $("#snowflake").click(makeSnowflake);
+  $("#gramophone").click(makeGramophone);
+  $("#head-massager").click(makeHeadMassager);
+  $("#spider").click(makeSpider);
 }
 
 function main() {
